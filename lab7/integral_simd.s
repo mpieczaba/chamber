b4669d20d4d724cc10ee58e6000ac018f7896489
Mikołaj Pieczaba <mikolaj@pieczaba.me>
2024-06-05 18:29:49 +0200

    refactor: improve integration performance in fpu_vs_simd

--- lab7/integral_simd.s
+++ lab7/integral_simd.s
@@ -7,6 +7,8 @@ n: .float 1000
 a: .float 5
 b: .float 20
 eight: .float 8
+four: .float 4
+mul_factor: .float 0, 1, 2, 3
 
 .text
 
@@ -18,28 +20,43 @@ integral_simd:
     movss a(%rip), %xmm1 # a
     movss b(%rip), %xmm2 # b
     movss n(%rip), %xmm3 # n
+
     subss %xmm1, %xmm2 # b - a
+    divss %xmm3, %xmm2 # (b - a) / n = k
+    
+    vbroadcastss %xmm2, %xmm2
 
-    divss %xmm3, %xmm2 # (b - a) / n
+    movups mul_factor(%rip), %xmm6 # [0, 1, 2, 3]
+    mulps %xmm2, %xmm6 # [0, k, 2k, 3k]
 
-    movss eight(%rip), %xmm5
+    movss eight(%rip), %xmm5 # [8, 0, 0, 0]
+    vbroadcastss %xmm5, %xmm5 # [8, 8, 8, 8]
 
-    mov $1000, %ecx
+    mov $250, %ecx # ecx = n / 4 = 1000 / 4 = 250
 
-    pxor %xmm0, %xmm0
+    pxor %xmm0, %xmm0 # [0, 0, 0, 0]
 
+    vbroadcastss %xmm1, %xmm1 # [x, x, x, x] 
+    addps %xmm6, %xmm1 # [x, x+k, x+2k, x+3k]
+
+    movups four(%rip), %xmm6 # [4, 0, 0, 0]
+    vbroadcastss %xmm6, %xmm6 # [4, 4, 4, 4]
+    mulps %xmm2, %xmm6 # [4k, 4k, 4k, 4k]
+ 
 sum_polygons:
-    # sum += k * (x^2 -2x + 8)
-    movaps %xmm1, %xmm3 # x
-    mulss %xmm1, %xmm3 # x*x
-    subss %xmm1, %xmm3 # x*x - x
-    subss %xmm1, %xmm3 # x*x - x - x
-    addss %xmm5, %xmm3 # x*x - x - x + 8
-    mulss %xmm2, %xmm3 # (x*x - x - x + 8) * k
+    movaps %xmm1, %xmm3
+
+    mulps %xmm1, %xmm3 # x*x
+    subps %xmm1, %xmm3 # x*x - x
+    subps %xmm1, %xmm3 # x*x - x - x
+    addps %xmm5, %xmm3 # x*x - x - x + 8
+
+    dpps $0xff, %xmm2, %xmm3 # (x*x - x - x + 8) * k
+
     addss %xmm3, %xmm0 # sum = (x*x - x - x + 8) * k + sum
-
-    addss %xmm2, %xmm1 # x = x + k
-
+    
+    addps %xmm6, %xmm1 # x = [x+4k, x+5k, x+6k, x+7k]
+    
     loop sum_polygons
 
 exit:
